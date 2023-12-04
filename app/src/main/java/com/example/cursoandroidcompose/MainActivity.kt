2e4a9b86dac263d5cad6bab67f454ddf63e48bef
Federico Navarro <navarrofederico14@gmail.com>
2023-12-04 08:16:00 -0300

    init commit , falta centrar.

--- app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.cursoandroidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cursoandroidcompose.ui.theme.CursoAndroidComposeTheme
 
@@ -19,10 +25,9 @@ class MainActivity : ComponentActivity() {
             CursoAndroidComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CheckScreen()
                 }
             }
         }
@@ -30,17 +35,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CheckScreen( modifier: Modifier = Modifier) {
+val image = painterResource(id = R.drawable.ic_task_completed)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Image(painter = image, contentDescription = "All task completed",modifier=modifier)
+        Text(text = stringResource(id = R.string.All_tasks_completed))
+        Text(text = stringResource(id = R.string.nice_work))
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:pixel_4", showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     CursoAndroidComposeTheme {
-        Greeting("Android")
+        CheckScreen()
     }
 }
