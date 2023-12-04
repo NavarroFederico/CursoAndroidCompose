c7ef80cbecc01bb6f193ed93c030fcfef7118f99
Federico Navarro <navarrofederico14@gmail.com>
2023-12-03 23:05:21 -0300

    Articulo de compose terminado

--- app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.cursoandroidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cursoandroidcompose.ui.theme.CursoAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    InstructionalScreen()
                 }
             }
         }
@@ -30,17 +39,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InstructionalScreen(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Image(painter = image, contentDescription = null, modifier = Modifier.fillMaxWidth())
+        TextBody()
+    }
+
 }
 
-@Preview(showBackground = true)
+@Composable
+fun TextBody(modifier: Modifier = Modifier) {
+    Text(text = stringResource(R.string.jetpack_compose_tutorial), modifier = modifier.padding(16.dp), fontSize = 24.sp)
+    Text(text = stringResource(R.string.content_text),Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+    Text(text = stringResource(R.string.content_text2),Modifier.padding(16.dp), textAlign = TextAlign.Justify )
+}
+
+@Preview(showBackground = true, device = "id:Nexus 5")
 @Composable
 fun GreetingPreview() {
     CursoAndroidComposeTheme {
-        Greeting("Android")
+        InstructionalScreen()
     }
 }
