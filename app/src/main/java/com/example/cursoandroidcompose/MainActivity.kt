a1dd4c4d23d89114a31f530637e470649e7e590d
Federico <navarrofederico14@gmail.com>
2023-12-05 15:21:16 -0300

    parcial

--- app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.cursoandroidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cursoandroidcompose.ui.theme.CursoAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +39,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun QuadrantScreen(name: String, modifier: Modifier = Modifier) {
+    Row (){
+        Quadrant(
+            stringResource(id = R.string.first_title),
+            stringResource(id = R.string.first_description),
+        )
+        Quadrant(
+            stringResource(id = R.string.second_title),
+            stringResource(id = R.string.second_description),
+        )
+    }
+    Row {
+        Quadrant(
+            stringResource(id = R.string.third_title),
+            stringResource(id = R.string.third_description),
+        )
+        Quadrant(
+            stringResource(id = R.string.fourth_title),
+            stringResource(id = R.string.fourth_description),
+        )
+    }
+
+}
+
+@Composable
+fun Quadrant(title: String, content: String, modifier: Modifier = Modifier) {
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(16.dp)
+
+    ) {
+        Text(text = title,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center)
+        Text(text = content, textAlign = TextAlign.Justify)
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CursoAndroidComposeTheme {
-        Greeting("Android")
+        Quadrant(stringResource(id = R.string.first_title),stringResource(id = R.string.first_description), )
     }
 }
