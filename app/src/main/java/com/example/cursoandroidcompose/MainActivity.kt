c868897b3a8fe28d140b56f3836a231adad2ebba
Federico <navarrofederico14@gmail.com>
2023-12-22 12:17:08 -0300

    Modificación final

--- app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Call
@@ -30,12 +28,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,10 +42,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CursoAndroidComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Surface(modifier = Modifier.fillMaxSize(), color = Color(0xFFDCF2DE)) {
                     BusinessCardApp(modifier = Modifier.fillMaxSize())
                 }
             }
@@ -60,46 +52,28 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun BusinessCardApp(modifier: Modifier = Modifier) {
-        Column(
-
-            verticalArrangement = Arrangement.SpaceAround,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            InfoProfile()
-            InfoContact()
+        Box(modifier = modifier) {
+            InfoProfile(modifier = Modifier.align(Alignment.Center))
+            InfoContact(modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 24.dp))
         }
     }
 
 
     @Composable
-    private fun InfoProfile() {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp),
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    private fun InfoProfile(modifier: Modifier = Modifier) {
+        Column(modifier = modifier.padding(horizontal = 8.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
             Image(
-                painter = painterResource(id = R.drawable.imagen_de_perfil_navarro_federico),
-                contentDescription = "Foto de perfil por defecto",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(120.dp)
-                    .clip(CircleShape),
+                    painter = painterResource(id = R.drawable.imagen_de_perfil_navarro_federico),
+                    contentDescription = "Foto de perfil por defecto",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                            .size(120.dp)
+                            .clip(CircleShape),
             )
-            Text(
-                text = "Navarro Federico",
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.titleLarge
-            )
-            Text(
-                text = "Android Developer ",
-                style = MaterialTheme.typography.titleMedium,
-                color = Color(0xFF3ddc84)
-            )
-
-
+            Text(text = "Navarro Federico", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.titleLarge, modifier = Modifier.padding(8.dp))
+            Text(text = "Android Developer ", style = MaterialTheme.typography.titleMedium, color = Color(0xFF3ddc84))
         }
 
     }
@@ -111,50 +85,115 @@ class MainActivity : ComponentActivity() {
         val typeIcon = Icons.Rounded
         val iconCall = typeIcon.Call
         val iconShare = typeIcon.Share
-        val iconGitHub = painterResource(id = R.drawable.icon_github)
         val iconEmail = typeIcon.Email
-
+        val tintColor = Color
         val urlGithub = "https://github.com/NavarroFederico"
 
-        Column(Modifier.verticalScroll(scrollState)) {
-            Item(iconCall, "+54 (11)23823504")
-            Item(iconGitHub, "My GitHub")
+        Column(modifier = modifier.verticalScroll(scrollState), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            Item(icon = iconCall, info = "+54 (11)23823504", contentDescription = "phone cel")
+            Item(icon = iconShare, info = "@navarrofede", contentDescription = "share")
+            Item(icon = iconEmail, info = "navarrofederico14@gmail.com", contentDescription = "email")
         }
 
     }
 
     @Composable
-    fun Item(icon: Any, info: String, modifier: Modifier = Modifier) {
-        var scrollState = rememberScrollState()
-        val uriHandler = LocalUriHandler.current
+    fun Item(icon: Any, info: String, contentDescription: String? = null, modifier: Modifier = Modifier) {
 
-        Row (modifier.horizontalScroll(scrollState)){
-
-            when (icon) {
-                is ImageVector -> {
-                    Icon(icon, contentDescription = null)
-                    Spacer(Modifier.size(16.dp))
-                    Text(text = info)
-                }
-
-                is Painter -> {
-                    Icon(painter = icon, contentDescription = null, modifier = Modifier.size(28.dp))
-                    Spacer(Modifier.size(16.dp))
-                    ClickableText(
-                        text = AnnotatedString(info),
-                        onClick = { uriHandler.openUri("https://github.com/NavarroFederico") })
-                }
-            }
+        Row() {
+            Icon(imageVector = icon as ImageVector, contentDescription = contentDescription, tint = Color(0xFF3ddc84))
+            Spacer(modifier = modifier.size(8.dp))
+            Text(text = info)
         }
 
-    }
+    }/*  @Composable
+      fun InfoContact(modifier: Modifier = Modifier) {
+          var scrollState = rememberScrollState()
 
+          val typeIcon = Icons.Rounded
+          val iconCall = typeIcon.Call
+          val iconShare = typeIcon.Share
+          val iconGitHub = painterResource(id = R.drawable.icon_github)
+          val iconEmail = typeIcon.Email
+
+          val urlGithub = "https://github.com/NavarroFederico"
+
+          Column(Modifier.verticalScroll(scrollState)) {
+              Item(iconCall, "+54 (11)23823504")
+              Item(iconGitHub, "My GitHub")
+              Item(iconShare,"@navarrofede")
+              Item(icon = iconEmail, info ="" )
+          }
+
+      }
+
+      @Composable
+      fun Item(icon: Any, info: String, modifier: Modifier = Modifier) {
+          var scrollState = rememberScrollState()
+          val uriHandler = LocalUriHandler.current
+
+          Row (modifier.horizontalScroll(scrollState)){
+
+              when (icon) {
+                  is ImageVector -> {
+                      Icon(icon, contentDescription = null)
+                      Spacer(Modifier.size(16.dp))
+                      Text(text = info)
+                  }
+
+                  is Painter -> {
+                      Icon(painter = icon, contentDescription = null, modifier = Modifier.size(28.dp))
+                      Spacer(Modifier.size(16.dp))
+                      ClickableText(
+                          text = AnnotatedString(info),
+                          onClick = { uriHandler.openUri("https://github.com/NavarroFederico") })
+                  }
+              }
+          }
+
+      }*/
+
+    /* @Composable
+   fun Item(icon: Any, info: String, link: String? = null, modifier: Modifier = Modifier) {
+       var scrollState = rememberScrollState()
+       val uriHandler = LocalUriHandler.current
+
+       Row(modifier.horizontalScroll(scrollState)) {
+           if (link != null) {
+               Icon(
+                   imageVector = if (icon is ImageVector) icon else Icons.Default.Email,
+                   painter = if (icon is Painter) icon else null,
+                   contentDescription = null,
+                   modifier = Modifier.size(28.dp).clickable { uriHandler.openUri(link) }
+               )
+           } else {
+               Icon(
+                   imageVector = if (icon is ImageVector) icon else Icons.Default.Email,
+                   painter = if (icon is Painter) icon else null,
+                   contentDescription = null,
+                   modifier = Modifier.size(28.dp)
+               )
+           }
+
+           Spacer(Modifier.size(16.dp))
+           if (link != null) {
+               ClickableText(
+                   text = AnnotatedString(info),
+                   onClick = { uriHandler.openUri(link) }
+               )
+           } else {
+               Text(text = info)
+           }
+       }
+   }*/
 
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
         CursoAndroidComposeTheme {
-            BusinessCardApp(Modifier.fillMaxSize())
+            Surface(modifier = Modifier.fillMaxSize(), color = Color(0xFFDCF2DE)) {
+                BusinessCardApp(Modifier.fillMaxSize())
+            }
         }
 
     }
