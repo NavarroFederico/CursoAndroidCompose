437cc7515b3fcde65006ee01cf12bb05ac4cb380
Federico <navarrofederico14@gmail.com>
2024-01-17 15:32:40 -0300

    En proceso

--- app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
@@ -1,33 +1,30 @@
 package com.example.cursoandroidcompose
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +46,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonApp() {
-    var descriptionVisibility by remember { androidx.compose.runtime.mutableStateOf(false) }
+    var currentStep by remember { mutableStateOf(1) }
+    val imageResource = when (currentStep) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+
+    val contentDescription = when (currentStep) {
+        1 -> stringResource(id = R.string.lemon_tree_content_description)
+        2 -> stringResource(id = R.string.lemon_content_description)
+        3 -> stringResource(id = R.string.glass_of_lemonade_content_description)
+        else -> stringResource(id = R.string.empty_glass_content_description)
+    }
+
+    val textDescription = when (currentStep) {
+        1 -> stringResource(id = R.string.Tap_the_lemon_tree_to_select_a_lemon)
+        2 -> stringResource(id = R.string.Keep_tapping_the_lemon_to_squeeze_it)
+        3 -> stringResource(id = R.string.Tap_the_lemonade_to_drink_it)
+        else -> {
+            stringResource(id = R.string.Tap_the_empty_glass_to_start_again)
+        }
+    }
     // A surface container using the 'background' color from the theme
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -61,50 +80,39 @@ fun LemonApp() {
             verticalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = painterResource(id = R.drawable.lemon_tree),
-                contentDescription = stringResource(
-                    id = R.string.lemon_tree_content_description
-                ),
+                painter = painterResource(imageResource),
+                contentDescription = contentDescription,
                 modifier = Modifier
+                    .wrapContentSize()
                     .border(
                         width = 2.dp,
                         color = Color(105, 205, 216),
                         shape = RoundedCornerShape(20.dp)
                     )
                     .background(color = Color(105, 205, 216), shape = RoundedCornerShape(20.dp))
-                    .pointerInput(Unit) {
-                        detectDragGesturesAfterLongPress(onDragStart = {
-                            descriptionVisibility = true
-                        }, onDrag = { change, dragAmount ->
-                            descriptionVisibility = false
-                            println("DRAGGING$ dragAmount")
+                    .clickable(onClickLabel = "Click Image", onClick = {
 
-                        })
-                    }
-                    .clickable(onClickLabel = "Click Image", onClick = { })
+                        when (currentStep) {
+                            1 -> currentStep = 2
+                            2 -> currentStep = 3
+                            3 -> currentStep = 4
+                            4 -> currentStep = 1
+
+                        }
+                    })
 
             )
 
             Spacer(modifier = Modifier.height(16.dp))
             Text(
-                text = stringResource(id = R.string.Tap_the_lemon_tree_to_select_a_lemon),
+                text = textDescription,
                 fontSize = 18.sp
             )
 
 
         }
-        if (descriptionVisibility) {
-            showDragDescription()
-        }
+
     }
-}
-
-
-@Composable
-fun showDragDescription() {
-    val description = stringResource(id = R.string.Tap_the_lemon_tree_to_select_a_lemon)
-    val activity = (LocalContext.current as Activity)
-    Toast.makeText(activity, description, Toast.LENGTH_SHORT).show()
 }
 
 @Preview(showBackground = true)
