8ad19de00d6249036eb3bd496ad933b75de20026
Federico Navarro <navarrofederico14@gmail.com>
2024-01-19 07:12:10 -0300

    TODO , mejorar logica

--- app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroidcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cursoandroidcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -38,14 +39,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeTheme {
-                LemonApp()
+                LemonAppV2()
             }
         }
     }
 }
 
 @Composable
-fun LemonApp() {
+fun LemonAppV1() {
     var currentStep by remember { mutableStateOf(1) }
     val imageResource = when (currentStep) {
         1 -> R.drawable.lemon_tree
@@ -69,6 +70,7 @@ fun LemonApp() {
             stringResource(id = R.string.Tap_the_empty_glass_to_start_again)
         }
     }
+
     // A surface container using the 'background' color from the theme
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -106,7 +108,7 @@ fun LemonApp() {
             Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = textDescription,
-                fontSize = 18.sp
+                fontSize = 18.sp,
             )
 
 
@@ -115,11 +117,102 @@ fun LemonApp() {
     }
 }
 
+@Composable
+fun LemonAppV2() {
+    var currentStep by remember { mutableStateOf(1) }
+    var randomNum by remember { mutableStateOf(1) }
+    var tapCount by remember { mutableStateOf(0) }
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        when (currentStep) {
+            1 -> LemonTextAndImage(
+                idText = R.string.Tap_the_lemon_tree_to_select_a_lemon,
+                idImageResource = R.drawable.lemon_tree,
+                idContentDescription = R.string.lemon_tree_content_description,
+                onStartClicked = {
+                    randomNum = (2..4).random()
+                    currentStep = 2
+                    Log.d("Log", "$tapCount $randomNum")
+
+                }
+            )
+
+            2 -> LemonTextAndImage(
+                idText = R.string.Keep_tapping_the_lemon_to_squeeze_it,
+                idImageResource = R.drawable.lemon_squeeze,
+                idContentDescription = R.string.lemon_content_description
+            ) {
+                Log.d("Log", "$tapCount $randomNum")
+
+                if (tapCount == randomNum) {
+                    currentStep = 3
+                    tapCount = 0
+                } else {
+                    tapCount++
+                    Log.d("Log", "$tapCount $randomNum")
+                }
+
+            }
+
+            3 -> LemonTextAndImage(
+                idText = R.string.Tap_the_lemonade_to_drink_it,
+                idImageResource = R.drawable.lemon_drink,
+                idContentDescription = R.string.glass_of_lemonade_content_description,
+                onStartClicked = { currentStep = 4 })
+
+            4 -> LemonTextAndImage(
+                idText = R.string.Tap_the_empty_glass_to_start_again,
+                idImageResource = R.drawable.lemon_restart,
+                idContentDescription = R.string.empty_glass_content_description,
+                onStartClicked = { currentStep = 1 })
+        }
+
+
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    idText: Int,
+    idImageResource: Int,
+    idContentDescription: Int,
+    onStartClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = idImageResource),
+            contentDescription = stringResource(id = idContentDescription),
+            modifier = Modifier
+                .wrapContentSize()
+                .border(
+                    width = 2.dp,
+                    color = Color(105, 205, 216),
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .background(color = Color(105, 205, 216), shape = RoundedCornerShape(20.dp))
+                .clickable(onClickLabel = "Click Image", onClick = onStartClicked)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(id = idText),
+            fontSize = 18.sp,
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LemonadeTheme {
-        LemonApp()
+        LemonAppV2()
     }
 }
 
