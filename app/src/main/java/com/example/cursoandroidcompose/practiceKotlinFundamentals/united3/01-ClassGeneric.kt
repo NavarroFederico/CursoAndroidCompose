df466d02a38b0e73e94587ed78eef2c3a9f2ffec
Federico Navarro <navarrofederico14@gmail.com>
2024-02-05 11:51:44 -0300

    Crear funciones de Extensión

--- app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/01-ClassGeneric.kt
+++ app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/01-ClassGeneric.kt
@@ -36,25 +36,26 @@ class Quiz {
 
     companion object StudentProgress {
         var total: Int = 10
-        var answered: Int = 3
+        var answered: Int = 6
+
     }
 }
+
 //las propiedades de extension no pueden almacenar datos, por lo que deben ser de solo acceso
 val Quiz.StudentProgress.progressText: String
     get() = "${answered} of ${total} answered"
 
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+
 fun main() {
 
-    /*    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-        val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-        val question3 =
-            Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-
-        println(question1.toString())
-        println(question2.toString())
-        println(question3.toString())
-        */
-    println(Quiz.progressText)
+    Quiz.printProgressBar()
 
 
 }
