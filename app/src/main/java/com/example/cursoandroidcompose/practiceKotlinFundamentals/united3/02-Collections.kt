dc552b12026ed55d34658e65c39104d29967d371
Federico Navarro <navarrofederico14@gmail.com>
2024-02-05 14:40:39 -0300

    Arrays en Kotlin

---
+++ app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/02-Collections.kt
@@ -0,0 +1,39 @@
+package com.example.cursoandroidcompose.practiceKotlinFundamentals.united3
+
+fun main() {
+    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+
+    val solarSystem = rockPlanets + gasPlanets
+
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
+
+    solarSystem[3] = "Little Earth"
+    println(solarSystem[3])
+
+    //Los arrays tienen un tamaño fijo. Una vez creados no se pueden agregar elementos nuevos.
+    //solarSystem[8] = "Pluto" //corre una
+    //Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 8
+
+    val newSolarSystem = arrayOf(
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune",
+        "Pluto"
+    )
+    println(newSolarSystem[8])
+
+}
