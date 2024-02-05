d48d5f9100520e7692256ced5d3bf3d07d9b6c74
Federico Navarro <navarrofederico14@gmail.com>
2024-02-05 18:03:35 -0300

    Collections of Maps

--- app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/02-Collections.kt
+++ app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/02-Collections.kt
@@ -2,29 +2,38 @@ package com.example.cursoandroidcompose.practiceKotlinFundamentals.united3
 
 fun main() {
 
-    //Crea un Set de planetas llamado solarSystem con mutableSetOf(). Esto muestra un MutableSet, cuya implementación predeterminada es LinkedHashSet().
-    val solarSystem =
-        mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    //key = planet , value = moons
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+    println(solarSystem.size)
+    //Agrego un valor nuevo a la coleccion map
+    solarSystem["Pluto"] = 5
 
     println(solarSystem.size)
 
-    solarSystem.add("Pluto")
+    //Busco el indice donde se encuentra el elemento con clave pluto
+    println(solarSystem["Pluto"])
 
-    println(solarSystem.size)
+    //tambien se puede acceder con get(). sino está devuelve un null
+    println(solarSystem.get("Theia"))
 
-    println(solarSystem.contains("Pluto"))
-
-    println("Pluto" in solarSystem)
-
-    //intentando agregar algo que ya existe
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
-
-    //remove un valor que toma por parametro
+    //remove
     solarSystem.remove("Pluto")
-
-    //consulta si pluto está
     println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+
+    /*La sintaxis de subíndice, o el método put(),
+    también puede modificar el valor de una clave que ya existe.
+    Usa la sintaxis de subíndice para actualizar las lunas de Júpiter a 78 y
+     muestra el nuevo valor.*/
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
 
 }
