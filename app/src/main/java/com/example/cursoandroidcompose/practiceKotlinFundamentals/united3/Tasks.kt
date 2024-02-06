79693e3c6f4b2620bdfc1fecf335e8faaf786a7e
Federico Navarro <navarrofederico14@gmail.com>
2024-02-06 10:14:29 -0300

    Task 4. Filter for short duration

--- app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/Tasks.kt
+++ app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/Tasks.kt
@@ -1,5 +1,6 @@
 package com.example.cursoandroidcompose.practiceKotlinFundamentals.united3
 
+
 enum class Daypart() {
     MORNING, AFTERNOON, EVENING
 }
@@ -11,13 +12,54 @@ data class Event(
     val durationInMinutes: Int
 )
 
+
 fun main() {
-    println(
+    val events = mutableListOf<Event>(
         Event(
-            title = "Study Kotlin",
-            description = "Commit to studying Kotlin atleast 15 minutes per day",
-            daypart = Daypart.EVENING,
+            title = "Wake up",
+            description = "Time to get up",
+            daypart = Daypart.MORNING,
+            durationInMinutes = 0
+        ),
+        Event(
+            title = "Eat breakfast",
+            daypart = Daypart.MORNING,
             durationInMinutes = 15
+        ),
+        Event(
+            title = "Learn about Kotlin",
+            daypart = Daypart.AFTERNOON,
+            durationInMinutes = 30
+        ),
+        Event(
+            title = "Practice Compose",
+            daypart = Daypart.AFTERNOON,
+            durationInMinutes = 60
+        ),
+        Event(
+            title = "Watch latest DevBytes video",
+            daypart = Daypart.AFTERNOON,
+            durationInMinutes = 10
+        ),
+        Event(
+            title = "Check out latest Android Jetpack library",
+            daypart = Daypart.EVENING,
+            durationInMinutes = 45
         )
     )
+    /*    var count: Int = 0
+        events.forEach {
+            println("Event ${++count}")
+            println(
+                "Title: ${it.title}, Daypart: ${it.daypart},durationMinutes ${it.durationInMinutes}"
+            )
+        }*/
+    val eventsShort = events.filter { it.durationInMinutes < 60 }
+    println("Count short event: ${eventsShort.size}")
+    eventsShort.forEach {
+        println()
+        println(
+            "Title: ${it.title}, Daypart: ${it.daypart},durationMinutes ${it.durationInMinutes}"
+        )
+    }
 }
