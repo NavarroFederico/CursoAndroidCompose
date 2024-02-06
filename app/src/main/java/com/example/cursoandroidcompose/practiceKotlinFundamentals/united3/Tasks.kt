d884778c2d0464b2801adddd49ce3e64ef89d71c
Federico Navarro <navarrofederico14@gmail.com>
2024-02-06 12:08:11 -0300

    Task 7. Propiedad de extension Corregida

--- app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/Tasks.kt
+++ app/src/main/java/com/example/cursoandroidcompose/practiceKotlinFundamentals/united3/Tasks.kt
@@ -1,6 +1,5 @@
 package com.example.cursoandroidcompose.practiceKotlinFundamentals.united3
 
-
 enum class Daypart() {
     MORNING, AFTERNOON, EVENING
 }
@@ -12,43 +11,57 @@ data class Event(
     val durationInMinutes: Int
 )
 
+val events = mutableListOf<Event>(
+    Event(
+        title = "Wake up",
+        description = "Time to get up",
+        daypart = Daypart.MORNING,
+        durationInMinutes = 0
+    ),
+    Event(
+        title = "Eat breakfast",
+        daypart = Daypart.MORNING,
+        durationInMinutes = 15
+    ),
+    Event(
+        title = "Learn about Kotlin",
+        daypart = Daypart.AFTERNOON,
+        durationInMinutes = 30
+    ),
+    Event(
+        title = "Practice Compose",
+        daypart = Daypart.AFTERNOON,
+        durationInMinutes = 60
+    ),
+    Event(
+        title = "Watch latest DevBytes video",
+        daypart = Daypart.AFTERNOON,
+        durationInMinutes = 10
+    ),
+    Event(
+        title = "Check out latest Android Jetpack library",
+        daypart = Daypart.EVENING,
+        durationInMinutes = 45
+    )
+)
+val Event.durationOfEvent: String
+    get() =
+        if (events[0].durationInMinutes < 60) {
+            "short"
+        } else {
+            "long"
+        }
+
 
 fun main() {
-    val events = mutableListOf<Event>(
-        Event(
-            title = "Wake up",
-            description = "Time to get up",
-            daypart = Daypart.MORNING,
-            durationInMinutes = 0
-        ),
-        Event(
-            title = "Eat breakfast",
-            daypart = Daypart.MORNING,
-            durationInMinutes = 15
-        ),
-        Event(
-            title = "Learn about Kotlin",
-            daypart = Daypart.AFTERNOON,
-            durationInMinutes = 30
-        ),
-        Event(
-            title = "Practice Compose",
-            daypart = Daypart.AFTERNOON,
-            durationInMinutes = 60
-        ),
-        Event(
-            title = "Watch latest DevBytes video",
-            daypart = Daypart.AFTERNOON,
-            durationInMinutes = 10
-        ),
-        Event(
-            title = "Check out latest Android Jetpack library",
-            daypart = Daypart.EVENING,
-            durationInMinutes = 45
-        )
-    )
+    //val [type name}.[property name] : data type
+    //property getter
+    //ejemplos
+    /*
+    val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+     */
 
-//Mostrar el último elemento de la lista events
-
-    println("Last event of the day: ${events.last().title}")
+    println("Duration of first event of the day: ${events[0].durationOfEvent}")
 }
+
