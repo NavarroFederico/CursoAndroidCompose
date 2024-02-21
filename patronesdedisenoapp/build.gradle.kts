39803f45e47bff00aea976fa8c86b0bdb1cf7ccf
Federico <navarrofederico14@gmail.com>
2024-02-21 10:51:45 -0300

    Se Creo el modulo Patrones de diseno y se cambió tema del proyecto

--- patronesdedisenoapp/build.gradle.kts
+++ patronesdedisenoapp/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cursoandroidcompose"
+    namespace = "com.example.patronesdedisenoapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.cursoandroidcompose"
+        applicationId = "com.example.patronesdedisenoapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
