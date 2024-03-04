da05f4207afcc19da9f518e68f5c92a4fb73f6b2
Federico <navarrofederico14@gmail.com>
2024-03-04 12:26:13 -0300

    Creación inicial de composables

--- patronesdedisenoapp/src/main/java/com/example/patronesdedisenoapp/PatternDesignScreen.kt
+++ patronesdedisenoapp/src/main/java/com/example/patronesdedisenoapp/PatternDesignScreen.kt
@@ -1,5 +1,43 @@
 package com.example.patronesdedisenoapp
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.patronesdedisenoapp.model.PatternDesign
+
+@Composable
+fun PatternDesignList(
+    patterns: List<PatternDesign>,
+    modifier: Modifier = Modifier,
+    contentPaddingValues: PaddingValues = PaddingValues(0.dp),
+) {
+
+}
+
+@Composable
+fun PatternDesignItem(patternDesign: PatternDesign, modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun PatternImage(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun PatterInformation() {
+
+}
+
+@Composable
+fun PatternItemButton(expanded: Boolean, onClick: () -> Unit, modifier: Modifier) {
+
+}
+
+@Composable
+fun InformationExtend(descriptionRes: Int, modifier: Modifier = Modifier) {
+
+}
 
 
