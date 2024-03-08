d4d549d36fe61ac70fb76e37742489e04f84fa81
Federico <navarrofederico14@gmail.com>
2024-03-08 16:43:28 -0300

    Se aadd listas y topbar

--- patronesdedisenoapp/src/main/java/com/example/patronesdedisenoapp/MainActivity.kt
+++ patronesdedisenoapp/src/main/java/com/example/patronesdedisenoapp/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.patronesdedisenoapp.model.PatternsDesignRepository
 import com.example.patronesdedisenoapp.ui.theme.PatternDesignAppTheme
@@ -24,10 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                        val patternDesign = PatternsDesignRepository.patterns[10]
-                        PatternDesignItem(patternDesign = patternDesign)
-
+                    PatternDesignApp()
                 }
             }
         }
@@ -35,9 +38,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun PatternDesignApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = { PatternDesignTopAppBar() }
+    ) {
+        val patternList = PatternsDesignRepository.patterns
+        PatternDesignList(patternsList = patternList, contentPaddingValues = it)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PatternDesignTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            titleContentColor = MaterialTheme.colorScheme.primaryContainer,
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
         modifier = modifier
     )
 }
@@ -46,6 +69,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PatternDesignAppTheme {
-        Greeting("Android")
+        PatternDesignList(patternsList = PatternsDesignRepository.patterns)
     }
 }
--- patronesdedisenoapp/src/main/java/com/example/patronesdedisenoapp/PatternDesignScreen.kt
+++ patronesdedisenoapp/src/main/java/com/example/patronesdedisenoapp/PatternDesignScreen.kt
@@ -3,7 +3,14 @@ package com.example.patronesdedisenoapp
 import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,6 +19,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
@@ -42,47 +56,65 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.patronesdedisenoapp.model.PatternDesign
 import com.example.patronesdedisenoapp.model.PatternsDesignRepository
+import com.example.patronesdedisenoapp.model.PatternsDesignRepository.patterns
 import com.example.patronesdedisenoapp.ui.theme.PatternDesignAppTheme
-import com.example.patronesdedisenoapp.ui.util.AnnotatedClickableText
 
-import com.example.patronesdedisenoapp.ui.util.AnnotatedClickableText
 import com.example.patronesdedisenoapp.ui.util.customSpanStyle
 
 @Composable
 fun PatternDesignList(
-    patterns: List<PatternDesign>,
+    patternsList: List<PatternDesign>,
     modifier: Modifier = Modifier,
     contentPaddingValues: PaddingValues = PaddingValues(0.dp),
 ) {
-
+    LazyColumn(contentPadding = contentPaddingValues)  {
+    items(patternsList){
+        patterns->
+        PatternDesignItem(patternDesign = patterns)
+    }
+    }
 }
 
 @Composable
 fun PatternDesignItem(patternDesign: PatternDesign, modifier: Modifier = Modifier) {
     var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.background
+        else MaterialTheme.colorScheme.secondaryContainer
+    )
 
-    Card(elevation = CardDefaults.cardElevation(defaultElevation = 2.dp), modifier = modifier) {
-
-        PatternImage(imageRes = patternDesign.imageRes)
-        Row(modifier = Modifier.padding(16.dp)) {
-            PatternInformation(
-                nameRes = patternDesign.nameRes,
-                patternDesignRes = patternDesign.patternTypeRes,
-                colorType = patternDesign.colorType,
-
+    Card(elevation = CardDefaults.cardElevation(defaultElevation = 2.dp), modifier = Modifier.padding(8.dp)) {
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
                 )
-            Spacer(modifier = Modifier.weight(1F))
-            PatternItemButton(
-                expanded = expanded,
-                onClick = { expanded = !expanded },
-                modifier = modifier
-            )
-        }
-        if (expanded) {
-            ExtendInformation(
-                descriptionRes = patternDesign.descriptionRes,
-                urlLink = patternDesign.url
-            )
+                .background(color = color)
+        ) {
+            PatternImage(imageRes = patternDesign.imageRes)
+            Row(modifier = Modifier.padding(16.dp)) {
+                PatternInformation(
+                    nameRes = patternDesign.nameRes,
+                    patternDesignRes = patternDesign.patternTypeRes,
+                    colorType = patternDesign.colorType,
+
+                    )
+                Spacer(modifier = Modifier.weight(1F))
+                PatternItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded },
+                    modifier = modifier
+                )
+            }
+            if (expanded) {
+                ExtendInformation(
+                    descriptionRes = patternDesign.descriptionRes,
+                    urlLink = patternDesign.url
+                )
+            }
         }
     }
 }
@@ -233,5 +265,51 @@ fun PatternDesignItemPreview() {
         PatternDesignItem(patternDesign = patternDesign)
     }
 
+}@Preview(showBackground = true)
+@Composable
+fun PatternListPreview() {
+    PatternDesignAppTheme {
+       val patternsList = patterns
+        PatternDesignList(patternsList = patternsList)
+    }
+
 }
 
+@Preview(showBackground = true)
+@Composable
+fun PruebaPreview() {
+    PatternDesignAppTheme {
+        val sections = (0 until 25).toList().chunked(5)
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            sections.forEachIndexed { index, items ->
+                item(span = { GridItemSpan(maxLineSpan) }) {
+                    Text(
+                        "This is section $index",
+                        Modifier
+                            .border(1.dp, Color.Gray)
+                            .height(80.dp)
+                            .wrapContentSize()
+                    )
+                }
+                items(
+                    items,
+                    // not required as it is the default
+                    span = { GridItemSpan(1) }
+                ) {
+                    Text(
+                        "Item $it",
+                        Modifier
+                            .border(1.dp, Color.Blue)
+                            .height(80.dp)
+                            .wrapContentSize()
+                    )
+                }
+            }
+        }
+
+    }
+}
