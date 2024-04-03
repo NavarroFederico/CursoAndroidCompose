2b15082960de3caddaad501427f9a757c9027190
Federico <navarrofederico14@gmail.com>
2024-04-03 14:59:47 -0300

    Se agregaron metodos para el viewModel para elegir una paralabra y desordenarla , para reiniciar el juego. Se agrega el arquitectura para el cambio de estado de la UI

--- 11_unscrambleapp/src/main/java/com/example/a11_unscrambleapp/ui/GameScreen.kt
+++ 11_unscrambleapp/src/main/java/com/example/a11_unscrambleapp/ui/GameScreen.kt
@@ -41,6 +41,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,12 +54,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a11_unscrambleapp.R
 import com.example.a11_unscrambleapp.ui.theme.UnscrambleTheme
 
 
 @Composable
-fun GameScreen() {
+fun GameScreen(
+    gameViewModel: GameViewModel = viewModel()
+) {
+    val gameUiState by gameViewModel.uiState.collectAsState()
+
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
     Column(
@@ -75,6 +82,10 @@ fun GameScreen() {
             style = typography.titleLarge,
         )
         GameLayout(
+            currentScrambledWord = gameUiState.currentScrambledWord,
+            userGuess = gameViewModel.userGuess,
+            onUserGuessChanged = { gameViewModel.updateUserGuess(it) },
+            onKeyboardDone = {},
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
@@ -127,7 +138,13 @@ fun GameStatus(score: Int, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun GameLayout(modifier: Modifier = Modifier) {
+fun GameLayout(
+    userGuess: String,
+    onUserGuessChanged: (String) -> Unit,
+    onKeyboardDone: () -> Unit,
+    currentScrambledWord: String,
+    modifier: Modifier = Modifier
+) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
     Card(
@@ -150,7 +167,7 @@ fun GameLayout(modifier: Modifier = Modifier) {
                 color = colorScheme.onPrimary
             )
             Text(
-                text = "scrambleun",
+                text = currentScrambledWord,
                 style = typography.displayMedium
             )
             Text(
@@ -159,7 +176,7 @@ fun GameLayout(modifier: Modifier = Modifier) {
                 style = typography.titleMedium
             )
             OutlinedTextField(
-                value = "",
+                value = userGuess,
                 singleLine = true,
                 shape = shapes.large,
                 modifier = Modifier.fillMaxWidth(),
@@ -168,14 +185,14 @@ fun GameLayout(modifier: Modifier = Modifier) {
                     unfocusedContainerColor = colorScheme.surface,
                     disabledContainerColor = colorScheme.surface,
                 ),
-                onValueChange = { },
+                onValueChange = onUserGuessChanged,
                 label = { Text(stringResource(R.string.enter_your_word)) },
                 isError = false,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
                 keyboardActions = KeyboardActions(
-                    onDone = { }
+                    onDone = { onKeyboardDone() }
                 )
             )
         }
--- 11_unscrambleapp/src/main/java/com/example/a11_unscrambleapp/ui/GameViewModel.kt
+++ 11_unscrambleapp/src/main/java/com/example/a11_unscrambleapp/ui/GameViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.a11_unscrambleapp.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.a11_unscrambleapp.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -8,19 +12,69 @@ import kotlinx.coroutines.flow.asStateFlow
 class GameViewModel : ViewModel() {
 
     // Game UI state
+    //Solo es accesible y editable desde GameViewModel
     private val _uiState = MutableStateFlow(GameUiState())
+
+    //Propiedad de solo lectura para IU se inicializa con el valor de _uiState.asStateFlow()
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 
+    //palabra desordenada actual
     private lateinit var currentWord: String
 
+    // Set of words used in the game
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    var userGuess by mutableStateOf("")
+        private set
+    init {
+        resetGame()
+    }
+
+    /**
+     * Devuelve una palabra desordenada obtenida desde una lista [allWords].
+     * Actualiza con la palabra usada la lista [usedWords]
+     *
+     * @return Devuelve una palabra desordenada.
+     */
     private fun pickRandomWordAndShuffle(): String {
         // Continue picking up a new random word until you get one that hasn't been used before
         currentWord = allWords.random()
+
         if (usedWords.contains(currentWord)) {
             return pickRandomWordAndShuffle()
         } else {
             usedWords.add(currentWord)
             return shuffleCurrentWord(currentWord)
         }
+    }
+
+    /**
+     * Desordena una palabra recibida por parametro.
+     *
+     * @param word Palabra para desordenar.
+     * @return Devuelve la palabra recibida [word], desordenada.
+     */
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    /**
+     * Reinicia el juego. Limpia [usedWords] y elige de nuevo una nueva palabra para que se inicie el juego.
+     *
+     */
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+
+    }
+
+    fun updateUserGuess(guessedWord: String) {
+        userGuess = guessedWord
     }
 }
