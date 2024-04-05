5e6a14ab90c4418ddcbf7192353a4e7bf72f5a7b
Federico Navarro <navarrofederico14@gmail.com>
2024-04-05 08:04:54 -0300

    preparaciòn de test

---
+++ 11_unscrambleapp/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -0,0 +1,13 @@
+package com.example.unscramble.ui.test
+
+import com.example.a11_unscrambleapp.ui.GameViewModel
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+
+    }
+}
